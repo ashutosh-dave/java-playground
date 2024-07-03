572702f2985c5b91691a7b6872ecf5a65676cb51
ashutosh-dave <ashutoshdave927@gmail.com>
2024-07-03 13:13:21 +0530

    Update commit

---
+++ fundamentals/variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args){
+        int myAge = 24;
+        double myHeight = 5.8;
+        char myInitial = 'A';
+        boolean isMale = true;
+        String myName = "Ashutosh";
+
+        System.out.println("My name is " + myName);
+        System.out.println("I am " + myAge + " years old");
+        System.out.println("I am " + myHeight + " feet tall");
+        System.out.println("My initial is " + myInitial);
+        if(isMale){
+            System.out.println("I am a male");
+        }
+    }    
+}
